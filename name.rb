ab6e884a1188d2b1a2ffd5f13021b6b3db59d14e
Andrew James <ajames20@gmail.com>
2016-12-09 11:24:25 +0200

    name.rb exercises complete

--- name.rb
+++ name.rb
@@ -16,6 +16,10 @@ You must do this without explicitly writing the puts method 10 times in
 a row. Hint: you can use the times method to do something repeatedly.
 =end
 
+
+10.times do |n|
+  puts first_name      # is a accessible here, in an inner scope?
+end
 puts "\n"
 
 
@@ -25,6 +29,11 @@ saves it into a variable, and then does the same for the last name.
 Then outputs their full name all at once.
 =end
 
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + last_name
 puts "\n"
 
 
@@ -50,8 +59,11 @@ What does x print to the screen in each case? Do
 they both give errors? Are the errors different? Why?
 =end
 
+puts "x will = 3 in the first block"
 puts "\n"
-
+puts "x is not accessible in the scope of the second block and will
+output an error"
+puts "\n"
 
 =begin
 What does the following error message tell you?
@@ -59,3 +71,6 @@ NameError: undefined local variable or method `shoes' for main:Object
   from (irb):3
   from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
 =end
+
+puts "This error message tells us that there is an undefined variable
+ in the program somewhere"
